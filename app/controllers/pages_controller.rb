fbda6cb83726abc4b12dbbf323b734ff1f17f4b9
jorge gonzalez <gonzalezjor13@gmail.com>
2025-04-30 16:33:51 -0500

    home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,9 @@
 class PagesController < ApplicationController
   def home
+    if params[:query].present?
+      @products = Product.includes(:reviews).search_products_by_name_price_or_discount(params[:query])
+    else
+    @products = Product.ordered.includes(:reviews).where(discount: 20..40).limit(8)
+    end
   end
 end
